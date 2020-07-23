18cbfed94568e42ad9949372e92d9d3ac0b37b97
vikrant karawande <viku1996@gmail.com>
2020-07-23 17:58:17 +0530

    Added Rank Component and Particles UI

--- src/App.js
+++ src/App.js
@@ -3,12 +3,30 @@ import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    number:{
+      value: 80,
+      density: {
+        enable: true,
+        value_area:800
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles"
+        params={particlesOptions}
+      />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
