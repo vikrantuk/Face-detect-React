9faf93e1e803e7d7e14846dac3cad9bfa72a9002
vikrant karawande <viku1996@gmail.com>
2020-08-03 17:03:26 +0530

    Heroku build

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -7,7 +7,7 @@ const Logo = () =>{
     return(
         <div className="ma4 mt0">
             <Tilt className="Tilt br2 shadow-2" options={{ max : 50 }} style={{ height: 100, width: 100 }} >
-                <div className="Tilt-inner pa3"> <img style={{paddingTop:'5px'}} src={brain}></img> </div>
+                <div className="Tilt-inner pa3"> <img alt="logo" style={{paddingTop:'5px'}} src={brain}></img> </div>
             </Tilt>
         </div>
     );
